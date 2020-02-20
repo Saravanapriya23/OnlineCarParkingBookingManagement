9059ce2eceff9af5285bf5fcbaac2b62c553c001
saravanapriyaksrct@gmail.com <saravanapriyaksrct@gmail.com>
2020-02-20 19:12:00 +0530

    Add project files.

---
+++ Controllers/CarOwnerDetailsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineCarParkingBookingManagement.Entity;
+using OnlineCarParkingBookingManagement.Repository;
+
+namespace OnlineCarParkingBookingManagement.Controllers
+{
+    public class CarOwnerDetailsController : Controller
+    {
+        // GET: CarOwnerDetails
+        CarOwnerDetailsRepository carownerdetails;
+        public CarOwnerDetailsController()
+        {
+            carownerdetails = new CarOwnerDetailsRepository();
+        }
+        public ActionResult Index()
+        {
+            IEnumerable<CarOwnerDetails> carOwnerDetails = carownerdetails.GetCarOwnerDetails();
+            return View(carownerdetails);
+        }
+        public ActionResult Display()
+        {
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        public ActionResult SignUp(CarOwnerDetails carOwnerInfo)
+        {
+            CarOwnerDetailsRepository.SignUp(carOwnerInfo);
+            return View();
+        }
+    }
+}
